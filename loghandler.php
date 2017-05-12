e81676201ebfed25bbcc8a969d7aae09f08babf6
James Fenn <TheAndroidMaster@users.noreply.github.com>
2017-05-12 18:26:34 -0400

    Update loghandler.php

--- loghandler.php
+++ loghandler.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html><html><body><?php
-header("Access-Control-Allow-Origin: http://theandroidmaster.github.io");
+header("Access-Control-Allow-Origin: https://theandroidmaster.github.io");
 $filename = $_GET["fchat"];
 if (empty($filename)) {
  $filename = "log";
